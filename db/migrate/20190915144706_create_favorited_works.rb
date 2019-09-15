288cfda557626ec7f079ae0f16672006f9d3e957
Peter Grant <jgrantps@hotmail.com>
2019-09-15 14:52:58 +0000

    migrate all db migration files 1st run

--- db/migrate/20190915144706_create_favorited_works.rb
+++ db/migrate/20190915144706_create_favorited_works.rb
@@ -1,6 +1,8 @@
 class CreateFavoritedWorks < ActiveRecord::Migration
   def change
-    t.integer :favorite_id
-    t.integer :work_id
+    create_table :favorited_works do |t|
+      t.integer :favorite_id
+      t.integer :work_id
+    end
   end
 end
