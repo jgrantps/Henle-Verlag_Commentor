e534d4a17acff4ec0edb58d24731ed6ff62ce42b
Peter Grant <jgrantps@hotmail.com>
2019-09-22 19:13:40 +0000

    add or remove from favorites list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     register Sinatra::Flash
-
     set :session_secret, "henle_verlag_urtext"
   end
 
@@ -16,9 +15,49 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/test' do
-    erb :"templates/unit_block"
+  get '/signup' do
+    erb :'user/signup'
   end
+
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to :"user/#{@user.slug}"
+    else
+      redirect to :'user/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      @user = User.find_by(session[:user_id])
+      redirect to :"user/#{@user.slug}"
+    else
+      erb :'user/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(name: params[:name])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to :"user/#{@user.slug}"
+    else
+      redirect to :"/login"
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect '/'
+    end
+  end
+
 
   helpers do
       # def signup_form_incomplete?(params)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,68 +1,30 @@
 class UsersController < ApplicationController
 
-get '/signup' do
-  erb :'user/signup'
-end
+  post '/user/post' do
+    binding.pry
+    @work = Work.find(params[:work_id])
+    @composer = @work.composer
+    binding.pry
 
-post '/signup' do
-  @user = User.new(params)
+    if params[:add_to_favorites?] == "on"
+      @favorite = Favorite.find_or_create_by(:user_id => current_user.id, :work_id => params[:work_id])
+    elsif params[:remove_from_favorites?] == "on"
+      @favorite = Favorite.find_by(:user_id => current_user.id, :work_id => params[:work_id])
+      @favorite.destroy
+    end
 
-  if @user.save
-    session[:user_id] = @user.id
-    redirect to :"user/#{@user.slug}"
-  else
-    redirect to :'user/signup'
-  end
-end
-
-get '/login' do
-  if logged_in?
-    @user = User.find_by(session[:user_id])
-    redirect to :"user/#{@user.slug}"
-  else
-    erb :'user/login'
-  end
-end
-
-post '/login' do
-  @user = User.find_by(name: params[:name])
-
-  if @user && @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect to :"user/#{@user.slug}"
-  else
-    redirect to :"/login"
-  end
-end
-
-post '/user/post' do
-  @work = Work.find(params[:work_id])
-  @composer = @work.composer
+    if !(params[:comments].empty?)
+      @comment = Comment.find_or_create_by(:content => params[:comments], :user_id => current_user.id, :work_id => params[:work_id])
+    end
   binding.pry
-
-  if params[:add_to_favorites?] == "on"
-    @favorite = Favorite.find_or_create_by(:user_id => current_user.id, :work_id => params[:work_id])
+    redirect to "/composer/feature/#{@composer.slug}"
   end
 
-  if !(params[:comments].empty?)
-    @comment = Comment.find_or_create_by(:content => params[:comments], :user_id => current_user.id, :work_id => params[:work_id])
-  end
-binding.pry
-  redirect to "/composer/feature/#{@composer.slug}"
-end
 
-get '/user/:slug' do
-  @user = User.find_by_slug(params[:slug])
-  erb :'user/profile'
-end
 
-get '/logout' do
-  if logged_in?
-    session.clear
-    redirect "/login"
-  else
-    redirect '/'
+  get '/user/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'user/profile'
   end
-end
 
 end
