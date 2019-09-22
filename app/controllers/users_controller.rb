d878e38b1532f89a94d8d111fabef353668ef494
Peter Grant <jgrantps@hotmail.com>
2019-09-22 20:48:51 +0000

    fixed HTML bug in iterated views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
 
   post '/user/post' do
-    # binding.pry
+
     @work = Work.find(params[:work_id])
     @composer = @work.composer
-    # binding.pry
 
     if params[:add_to_favorites?] == "on"
       @favorite = Favorite.find_or_create_by(:user_id => current_user.id, :work_id => params[:work_id])
@@ -16,7 +15,7 @@ class UsersController < ApplicationController
     if !(params[:comments].empty?)
       @comment = Comment.find_or_create_by(:content => params[:comments], :user_id => current_user.id, :work_id => params[:work_id])
     end
-  # binding.pry
+
     redirect to "/user/#{@current_user.slug}"
   end
 
