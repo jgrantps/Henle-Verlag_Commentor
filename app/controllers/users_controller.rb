e068e125a94f83180d8f660c52ac8bf2a062a73c
Peter Grant <jgrantps@hotmail.com>
2019-09-23 15:50:33 +0000

    wired up the delete method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   post '/user/edit' do
     @comment=Comment.find(params[:comment_id])
     # binding.pry
-    if @comment.user_id == current_user.id
+    if @comment.user == current_user
       @work = Work.find(@comment.work_id)
       erb :"user/edit"
     else
@@ -33,6 +33,37 @@ class UsersController < ApplicationController
       redirect to "/user/#{@current_user.slug}"
     end
   end
+
+  patch '/user/edit' do
+     binding.pry
+    if logged_in?
+      @comment=Comment.find(params[:comment_id])
+        if @comment.user == current_user && !params[:comment].empty?
+          @comment.update(content: params[:comment])
+          redirect to :"/user/#{current_user.slug}"
+        elsif logged_in? && params[:comment].empty?
+          redirect to :"/user/#{current_user.slug}"
+        else
+          redirect to :"/"
+        end
+      end
+  end
+
+
+  delete '/user/edit' do
+    binding.pry
+    @comment = Comment.find(params[:comment_id])
+    if @comment.user == current_user
+        @comment.delete
+        redirect to :"/user/#{current_user.slug}"
+
+    elsif logged_in?
+      redirect to :"/user/#{current_user.slug}"
+    else
+    redirect to :"/"
+    end
+  end
+
 
   get '/user/:slug' do
     @user = User.find_by_slug(params[:slug])
